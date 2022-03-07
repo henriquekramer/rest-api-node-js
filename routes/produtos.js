4274dd44288f6a0221b01941d1fa1b4ea7adf801
Henrique <henriquemkramer@gmail.com>
2022-03-07 18:34:49 -0300

    possibiliando adicionar mais imagens a um mesmo produto

--- routes/produtos.js
+++ routes/produtos.js
@@ -45,4 +45,9 @@ router.patch('/', login.obrigatorio, ProdutosController.updateProduto);
 // DELETA UM PRODUTO
 router.delete('/', login.obrigatorio, ProdutosController.deleteProduto);
 
+// IMAGENS
+router.post('/:id_produto/imagem', login.obrigatorio, upload.single('produto_imagem'), ProdutosController.postImagem);
+router.get('/:id_produto/imagens', ProdutosController.getImagens);
+
+
 module.exports = router; //para exportar o router
